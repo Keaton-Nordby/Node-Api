36d81a5310ddc063071aab13a36b8e2a1785f217
Keaton Nordby <keatonnordby4@gmail.com>
2025-04-26 17:39:29 -0500

    initial project for express work

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// declaring a route
+app.get('/', (req, res) => {
+    res.send("Hello Node api");
+})
+
+
+
+app.listen(port, () => {
+    console.log(`Node api app is running on port ${port}`);
+})

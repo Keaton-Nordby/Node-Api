a2d965bccb6ada4e1777d6655e0d2f1e87379019
Keaton Nordby <keatonnordby4@gmail.com>
2025-04-27 19:22:17 -0500

    Small progress

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
-const port = 3000;
 
 // declaring a route
 app.get('/', (req, res) => {
-    res.send("Hello Node api");
-})
+    res.send("Hello Node API");
+});
 
-
-
-app.listen(port, () => {
-    console.log(`Node api app is running on port ${port}`);
-})
+mongoose.set('strictQuery', false)
+mongoose.connect('mongodb+srv://root:root@nodeapi.iqrylev.mongodb.net/node-API?retryWrites=true&w=majority&appName=NODEAPI')
+    .then(() => {
+        console.log("Connected to MongoDB");
+        app.listen(3000, () => {
+            console.log('Node API app is running on port 3000');
+        });
+    })
+    .catch((error) => {
+        console.log(error);
+    });
